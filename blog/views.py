0cc1d6b3195a15e6d6b363c19f27bb8d88261f08
Agustin Orozco <tatuorozco@gmail.com>
2025-10-18 11:46:41 -0300

    resuelto inconveniente con archivo README

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AutorForm, CategoriaForm, PostForm, BuscarForm
+from .models import Post
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def crear_autor(request):
+    form = AutorForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, 'autor_form.html', {'form': form})
+
+def crear_categoria(request):
+    form = CategoriaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, 'categoria_form.html', {'form': form})
+
+def crear_post(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request, 'post_form.html', {'form': form})
+
+def buscar_post(request):
+    form = BuscarForm()
+    resultados = None
+    if request.method == 'POST':
+        form = BuscarForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            resultados = Post.objects.filter(titulo__icontains=query)
+    return render(request, 'resultados.html', {'form': form, 'resultados': resultados})
